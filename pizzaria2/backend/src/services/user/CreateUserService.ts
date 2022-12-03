354c7ac040d4ce51aca05bd22235936b9356c85d
Adriano <adriano_alves_2008@hotmail.com>
2022-12-03 11:12:36 -0300

    cadastrando user

--- pizzaria2/backend/src/services/user/CreateUserService.ts
+++ pizzaria2/backend/src/services/user/CreateUserService.ts
@@ -1,12 +1,44 @@
+import prismaClient from '../../prisma'
+
 interface UserRequest{
-  name: String;
-  email: String;
-  password: String
+  name: string;
+  email: string;
+  password: string
 }
 
 class CreateUserService{
   async execute ({name, email, password}:UserRequest){
-    return {name: name}
+
+    //verificar se ele enviou um email
+    if(!email){
+      throw new Error("Email incorreto")
+    }
+
+    //verificar se esse email já está cadastrado na plataforma
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    })
+    if(userAlreadyExists){
+      throw new Error('Email já está cadastrado')
+    }
+
+    const user = await prismaClient.user.create({
+      data:{
+        name: name,
+        email: email,
+        password: password
+      },
+      //O select é para informar o que você quer devolver, principalmento por questões de segurança, como por exemplo não devolver a senha
+      select:{
+        id:true,
+        email:true,
+        name:true
+      }
+    })
+
+    return {user}
   }
 }
 

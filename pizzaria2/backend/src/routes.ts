ffb97e17e0c0ae7dc315efd34fd7524a5aa2b7d7
Adriano <adriano_alves_2008@hotmail.com>
2022-12-06 10:00:01 -0300

    listando categorias

--- pizzaria2/backend/src/routes.ts
+++ pizzaria2/backend/src/routes.ts
@@ -5,6 +5,7 @@ import {AuthUserController} from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 
 import {CreateCategoryController} from './controllers/category/CreateCategoryController'
+import {ListCategoryController} from './controllers/category/ListCategoryController'
 
 import {isAuthenticated} from './middlewares/isAuthenticated'
 
@@ -20,5 +21,6 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 //Rotas Category
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
 
 export {router}

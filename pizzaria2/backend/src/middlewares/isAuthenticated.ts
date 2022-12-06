e1c7064142818d82b46d4e02a3d7ef56ba954b3f
Adriano <adriano_alves_2008@hotmail.com>
2022-12-06 09:25:34 -0300

    detalhes do usuário

--- pizzaria2/backend/src/middlewares/isAuthenticated.ts
+++ pizzaria2/backend/src/middlewares/isAuthenticated.ts
@@ -25,6 +25,9 @@ export function isAuthenticated(
       token,
       process.env.JWT_SECRET
     ) as Payload
+
+    //Recuperar o id do token e colocar dentro de uma variável user_id dentro do Req
+    req.user_id = sub
     return next()
   } catch (err) {
     return res.status(401).end()

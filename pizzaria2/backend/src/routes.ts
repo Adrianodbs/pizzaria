5b850e87413250d8d5a98156fffc916d13593020
Adriano <adriano_alves_2008@hotmail.com>
2022-12-07 20:36:12 -0300

    funcionalidade para concluir pedido

--- pizzaria2/backend/src/routes.ts
+++ pizzaria2/backend/src/routes.ts
@@ -18,6 +18,7 @@ import {RemoveItemController} from './controllers/order/RemoveItemController'
 import {SendOrderController} from './controllers/order/SendOrderController'
 import {ListOrderController} from './controllers/order/ListOrderController'
 import {DetailOrderController} from './controllers/order/DetailOrderController'
+import {FinishOrderController} from './controllers/order/FinishOrderController'
 
 import {isAuthenticated} from './middlewares/isAuthenticated'
 
@@ -54,6 +55,7 @@ router.delete('/order/remove', isAuthenticated, new RemoveItemController().handl
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrderController().handle)
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 
 export {router}

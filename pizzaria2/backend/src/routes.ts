e10847c8de471cccd46b8c73ceac09a304352e9a
Adriano <adriano_alves_2008@hotmail.com>
2022-12-07 09:06:54 -0300

    fechando um pedido

--- pizzaria2/backend/src/routes.ts
+++ pizzaria2/backend/src/routes.ts
@@ -12,6 +12,7 @@ import {CreateProductController} from './controllers/product/CreateProductContro
 import {ListByCategoryController} from './controllers/product/ListByCategoryController'
 
 import {CreateOrderController} from './controllers/order/CreateOrderController'
+import {RemoveOrderController} from './controllers/order/RemoveOrderController'
 
 import {isAuthenticated} from './middlewares/isAuthenticated'
 
@@ -40,6 +41,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 //Rotas Order
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 
 export {router}

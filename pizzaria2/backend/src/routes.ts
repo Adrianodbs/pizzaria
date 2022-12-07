cd64d03546bd16c08e1ad33487c658c4aaf737ab
Adriano <adriano_alves_2008@hotmail.com>
2022-12-07 20:02:25 -0300

    enviando pedido

--- pizzaria2/backend/src/routes.ts
+++ pizzaria2/backend/src/routes.ts
@@ -15,6 +15,7 @@ import {CreateOrderController} from './controllers/order/CreateOrderController'
 import {RemoveOrderController} from './controllers/order/RemoveOrderController'
 import {AddItemController} from './controllers/order/AddItemController'
 import {RemoveItemController} from './controllers/order/RemoveItemController'
+import {SendOrderController} from './controllers/order/SendOrderController'
 
 import {isAuthenticated} from './middlewares/isAuthenticated'
 
@@ -47,6 +48,8 @@ router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 
 export {router}

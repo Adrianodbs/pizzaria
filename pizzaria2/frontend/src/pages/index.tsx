e018080c211ad922069179860639d757539a25a3
Adriano <adriano_alves_2008@hotmail.com>
2022-12-08 11:14:26 -0300

    finalizando tela de login

--- pizzaria2/frontend/src/pages/index.tsx
+++ pizzaria2/frontend/src/pages/index.tsx
@@ -28,6 +28,8 @@ export default function Home() {
               Acessar
             </Button>
           </form>
+
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
         </div>
       </div>
     </>

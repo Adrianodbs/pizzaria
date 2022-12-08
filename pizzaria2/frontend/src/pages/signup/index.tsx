3a1e15d59fa83e2c960278189b6b1e38dd55a0ff
Adriano <adriano_alves_2008@hotmail.com>
2022-12-08 11:23:37 -0300

    criando página de cadastro

--- pizzaria2/frontend/src/pages/signup/index.tsx
+++ pizzaria2/frontend/src/pages/signup/index.tsx
@@ -1,37 +1,43 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../../styles/home.module.scss'
+import styles from '../../../styles/home.module.scss'
 
-import logoImg from '../../public/logo.svg'
-import {Input} from '../components/ui/Input'
-import { Button } from '../components/ui/Button'
+import logoImg from '../../../public/logo.svg'
+import {Input} from '../../components/ui/Input'
+import { Button } from '../../components/ui/Button'
 
-export default function Home() {
+export default function Signup() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça o seu login</title>
+        <title>Faça o seu cadastro agora</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt='Logo Sujeito Pizzaria' />
         <div className={styles.login}>
+          <h1>Criando a sua conta</h1>
           <form>
+            <Input 
+              placeholder='Digite o seu nome' 
+              type='text' 
+            />
             <Input 
               placeholder='Digite o seu E-mail' 
               type='text' 
             />
+           
             <Input placeholder='Digite a sua senha' type='password' />
             <Button 
               type='submit'
               Loading={false}
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link  className={styles.text} href='/signup'>
-            Não possui uma conta? Cadastre-se
+          <Link  className={styles.text} href='/'>
+            Já possui uma conta? Faça login.
           </Link>
           
         </div>

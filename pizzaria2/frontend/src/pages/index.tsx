830896b27298a6b277cf7c8d7d88e15ad70a1387
Adriano <adriano_alves_2008@hotmail.com>
2022-12-08 13:33:36 -0300

    criando contexto

--- pizzaria2/frontend/src/pages/index.tsx
+++ pizzaria2/frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import {useContext, FormEvent} from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -7,7 +9,23 @@ import logoImg from '../../public/logo.svg'
 import {Input} from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
 
+import {AuthContext} from '../contexts/AuthContext'
+
 export default function Home() {
+
+  const {signIn} = useContext(AuthContext)
+
+  async function handleLogin(event: FormEvent){
+
+    event.preventDefault()
+
+    let data = {
+      email: "teste@teste.com",
+      password: "123123"
+    }
+
+    await signIn(data)
+  }
   return (
     <>
       <Head>
@@ -16,7 +34,7 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt='Logo Sujeito Pizzaria' />
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input 
               placeholder='Digite o seu E-mail' 
               type='text' 

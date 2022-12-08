a8bf77b92e4e750e2b3e6885f63eed52509d378c
Adriano <adriano_alves_2008@hotmail.com>
2022-12-08 17:08:37 -0300

    configurando api

--- pizzaria2/frontend/src/contexts/AuthContext.tsx
+++ pizzaria2/frontend/src/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
 import {createContext, ReactNode, useState} from 'react'
 
+import {destroyCookie} from 'nookies'
+import Router from 'next/router'
+
 type AuthContextData = {
   user: UserProps;
   isAuthenticated:boolean;
-  signIn: (credentials: SignInProps)=> Promise <void>
+  signIn: (credentials: SignInProps)=> Promise <void>;
+  signOut: ()=> void
 }
 
 type UserProps ={
@@ -24,6 +28,15 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+export function signOut(){
+  try {
+    destroyCookie(undefined, '@nextauth.token')
+    Router.push('/')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export function AuthProvider({children}:AuthProviderProps){
 
   const [user,setUser] = useState<UserProps>()
@@ -35,7 +48,7 @@ export function AuthProvider({children}:AuthProviderProps){
     console.log(password)
   }
   return(
-    <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
+    <AuthContext.Provider value={{user, isAuthenticated, signIn, signOut}}>
       {children}
     </AuthContext.Provider>
   )

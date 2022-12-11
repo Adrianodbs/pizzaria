9aead9f50b35e14c292eee6214f0c56f5599e9f6
Adriano <adriano_alves_2008@hotmail.com>
2022-12-11 12:03:33 -0300

    rotas privadas pt2

--- pizzaria2/frontend/src/pages/dashboard/index.tsx
+++ pizzaria2/frontend/src/pages/dashboard/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import {canSSRAuth} from '../../utils/canSSRAuth'
 
 export default function Dashboard() {
   return (
     <div>Dashboard</div>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx)=>{
+
+  return{
+    props:{}
+  }
+})

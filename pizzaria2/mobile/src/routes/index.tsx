6f888c0379904daa4288615380fbc33b6d1ac2ae
Adriano <adriano_alves_2008@hotmail.com>
2022-12-19 10:39:09 -0300

    configurando navegação

---
+++ pizzaria2/mobile/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {View} from 'react-native'
+
+import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
+
+function Routes(){
+
+  const isAuthenticated = false
+  const loading = false
+  return(
+    isAuthenticated ? <AppRoutes /> : <AuthRoutes />
+  )
+}
+
+export default Routes

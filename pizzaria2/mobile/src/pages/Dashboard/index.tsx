f1c76464961bd63e83bd686717774d7c8382c856
Adriano <adriano_alves_2008@hotmail.com>
2022-12-21 21:48:34 -0300

    tela novo pedido

--- pizzaria2/mobile/src/pages/Dashboard/index.tsx
+++ pizzaria2/mobile/src/pages/Dashboard/index.tsx
@@ -1,14 +1,59 @@
-import React, {useContext} from 'react'
-import {View, Text, Button} from 'react-native'
+import React from 'react'
+import {Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet} from 'react-native'
 
-import { AuthContext } from '../../contexts/AuthContext'
+
 
 export default function Dashboard() {
-  const {signOut} = useContext(AuthContext)
+
   return (
-    <View>
-      <Text>Dashboard</Text>
-      <Button title='Sair do app' onPress={signOut} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Novo pedido</Text>
+
+      <TextInput style={styles.input} placeholder='Número da mesa' placeholderTextColor='#f0f0f0' keyboardType='numeric' />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Abrir mesa</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    paddingVertical:15,
+    backgroundColor:'#1d1d2e'
+  },
+  title:{
+    fontSize:30,
+    fontWeight:'bold',
+    color:'#fff',
+    marginBottom:24,
+  },
+  input:{
+    width:'90%',
+    height:60,
+    backgroundColor:'#101026',
+    borderRadius:4,
+    paddingHorizontal:8,
+    textAlign:'center',
+    fontSize:22,
+    color:'#fff'
+  },
+  button:{
+    width:'90%',
+    height:40,
+    backgroundColor:'#3fffa3',
+    borderRadius:4,
+    marginVertical:12,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  buttonText:{
+    fontSize:18,
+    color:'#101026',
+    fontWeight:'bold'
+  }
+})

0614c46c87015b08b07c142bee511d6970a2b0e3
Adriano <adriano_alves_2008@hotmail.com>
2022-12-22 21:23:32 -0300

    recebendo parametros

--- pizzaria2/mobile/src/pages/Order/index.tsx
+++ pizzaria2/mobile/src/pages/Order/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
+import { useRoute, RouteProp } from '@react-navigation/native'
+
+type RouteDetailParams = {
+  Order: {
+    number: string | number
+    order_id: string
+  }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
+
 export default function Order() {
+  const route = useRoute<OrderRouteProps>()
   return (
     <View style={styles.container}>
       <Text>Tela order</Text>
+      <Text>{route.params.number}</Text>
     </View>
   )
 }

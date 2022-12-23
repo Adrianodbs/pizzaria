915536165098a5bd5d2f9c7b1e23079cb463fa99
Adriano <adriano_alves_2008@hotmail.com>
2022-12-22 21:40:18 -0300

    tela order (pedido)

--- pizzaria2/mobile/src/pages/Order/index.tsx
+++ pizzaria2/mobile/src/pages/Order/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput
+} from 'react-native'
 
 import { useRoute, RouteProp } from '@react-navigation/native'
+
+import { Feather } from '@expo/vector-icons'
 
 type RouteDetailParams = {
   Order: {
@@ -16,12 +24,71 @@ export default function Order() {
   const route = useRoute<OrderRouteProps>()
   return (
     <View style={styles.container}>
-      <Text>Tela order</Text>
-      <Text>{route.params.number}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>Mesa {route.params.number}</Text>
+        <TouchableOpacity>
+          <Feather name="trash-2" size={28} color="#ff3f4b" />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: '#fff' }}>Pizzas</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.input}>
+        <Text style={{ color: '#fff' }}>Pizza de calabresa</Text>
+      </TouchableOpacity>
+
+      <View style={styles.qtdContainer}>
+        <Text style={styles.qtdText}>Quantidade</Text>
+        <TextInput
+          style={[styles.input, { width: '60%', textAlign: 'center' }]}
+          placeholderTextColor="#f0f0f0"
+          keyboardType="numeric"
+        />
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    backgroundColor: '#1d1d2e',
+    paddingVertical: '5%',
+    paddingEnd: '4%',
+    paddingStart: '4%'
+  },
+  header: {
+    flexDirection: 'row',
+    marginBottom: 12,
+    alignItems: 'center',
+    marginTop: 24
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginRight: 14
+  },
+  input: {
+    backgroundColor: '#101026',
+    borderRadius: 4,
+    width: '100%',
+    height: 40,
+    marginBottom: 12,
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+    color: '#fff',
+    fontSize: 20
+  },
+  qtdContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  qtdText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff'
+  }
 })
